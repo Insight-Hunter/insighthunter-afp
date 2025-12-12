f114eeddb360f974a1c5877bdb7b10f396b4acb5
James Michael Hunter Turner <michael@turneratlanta.com>
2025-12-12 14:29:13 -0500

    Update reportSignedUrl.ts

--- workers/reportSignedUrl.ts
+++ workers/reportSignedUrl.ts
@@ -1,6 +1,6 @@
 // workers/reportSignedUrl.ts
 import { verifyAccessJwt, requireRole, isSubject } from "./utils/auth";
-import "./utils/presign";
+import reportSignedUrl from "./utils/presign";
 
 export default {
   async fetch(request: Request, env: any): Promise<Response> {
