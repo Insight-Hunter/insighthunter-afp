324aed161d404c762fe3f88d2343c65f0cbde693
James Michael Hunter Turner <michael@turneratlanta.com>
2025-12-12 19:46:40 -0500

    Update and rename layout.tsx to Layout.tsx

---
+++ frontend/app/Layout.tsx
@@ -0,0 +1,10 @@
+import "../styles/globals.scss";
+
+<header className="app-header">
+  <div className="logo">INSIGHTHUNTER</div>
+  <nav>
+    <a href="/company">Start Wizard</a>
+    <a href="/admin">View Reports</a>
+    <a href="/preview">Simulation Preview</a>
+  </nav>
+</header>
